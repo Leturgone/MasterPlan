f55f2ef277fa1a24e7cddd67203b2d618dbe1f63
Leturgone <finekon221@gmail.com>
2026-04-02 20:34:12 +0300

    add TaskRepository

---
+++ app/src/main/java/com/app/masterplan/domain/repository/remote/TaskRepository.kt
@@ -0,0 +1,15 @@
+package com.app.masterplan.domain.repository.remote
+
+import com.app.masterplan.domain.dto.NewTaskData
+import com.app.masterplan.domain.model.plans.TaskStatus
+import java.util.UUID
+
+interface TaskRepository {
+
+    suspend fun addTaskToPlan(planId: UUID, newTask: NewTaskData): UUID
+
+    suspend fun updateTaskStatus(taskId: UUID, status: TaskStatus): UUID
+
+    suspend fun deleteTask(taskId: UUID): UUID
+
+}
