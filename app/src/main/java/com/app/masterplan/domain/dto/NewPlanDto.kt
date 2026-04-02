9f165703e2005e9c4d6b50bdedd3cc2c7c1dcc5a
Leturgone <finekon221@gmail.com>
2026-04-02 20:34:50 +0300

    add NewPlanDto

---
+++ app/src/main/java/com/app/masterplan/domain/dto/NewPlanDto.kt
@@ -0,0 +1,13 @@
+package com.app.masterplan.domain.dto
+
+import java.time.LocalDate
+import java.util.UUID
+
+data class NewPlanDto(
+    val title: String,
+    val description: String,
+    val startDate: LocalDate? = null,
+    val endDate: LocalDate,
+    val directorId: UUID,
+    val document: AttachedDocument? = null
+)
