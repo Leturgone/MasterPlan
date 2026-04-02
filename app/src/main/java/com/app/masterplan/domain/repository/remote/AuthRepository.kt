2251a1f4ae65bc2670c588fdcc9d1687121b6f3b
Leturgone <finekon221@gmail.com>
2026-04-02 14:50:15 +0300

    add AuthRepository

---
+++ app/src/main/java/com/app/masterplan/domain/repository/remote/AuthRepository.kt
@@ -0,0 +1,7 @@
+package com.app.masterplan.domain.repository.remote
+
+import com.app.masterplan.domain.model.auth.JwtToken
+
+interface AuthRepository {
+    suspend fun login(login: String, password: String): JwtToken
+}
