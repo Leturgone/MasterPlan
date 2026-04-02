5ddcd02df32d59414cf7a91c05fa601b9e04a08a
Leturgone <finekon221@gmail.com>
2026-04-02 21:06:23 +0300

    add GetAssignedTasksUseCase

---
+++ app/src/main/java/com/app/masterplan/domain/useacse/plans/GetAssignedTasksUseCase.kt
@@ -0,0 +1,18 @@
+package com.app.masterplan.domain.useacse.plans
+
+import com.app.masterplan.domain.model.plans.Task
+import com.app.masterplan.domain.repository.remote.TaskRepository
+import java.util.UUID
+
+class GetAssignedTasksUseCase(
+    private val taskRepository: TaskRepository
+) {
+    suspend operator fun invoke(executorId: UUID): Result<List<Task>>{
+        return try {
+            val tasks = taskRepository.getAssignedTasks(executorId)
+            Result.success(tasks)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
