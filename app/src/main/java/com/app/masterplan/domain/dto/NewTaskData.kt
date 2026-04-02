204619bd1767ad455352b99713ab93ebc4f222bb
Leturgone <finekon221@gmail.com>
2026-04-02 20:35:24 +0300

    add NewTaskData

---
+++ app/src/main/java/com/app/masterplan/domain/dto/NewTaskData.kt
@@ -0,0 +1,12 @@
+package com.app.masterplan.domain.dto
+
+import java.time.LocalDate
+import java.util.UUID
+
+data class NewTaskData(
+    val title: String,
+    val description: String,
+    val endDate: LocalDate,
+    val document: AttachedDocumentDto? = null,
+    val executorsId: MutableList<UUID>
+)
