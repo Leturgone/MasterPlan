1830d5bad351ec3421a877a7825004289fefee08
Leturgone <finekon221@gmail.com>
2026-04-02 17:51:55 +0300

    add DownloadFileUseCase

---
+++ app/src/main/java/com/app/masterplan/domain/useacse/files/DownloadFileUseCase.kt
@@ -0,0 +1,17 @@
+package com.app.masterplan.domain.useacse.files
+
+import com.app.masterplan.domain.repository.remote.DocumentRepository
+import java.util.UUID
+
+class DownloadFileUseCase(
+    private val documentRepository: DocumentRepository
+) {
+    suspend operator fun invoke(documentId: UUID): Result<ByteArray>{
+        return try {
+            val documentByteArray = documentRepository.downloadDocument(documentId)
+            Result.success(documentByteArray)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
