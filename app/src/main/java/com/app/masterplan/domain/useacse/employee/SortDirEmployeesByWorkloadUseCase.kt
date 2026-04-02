e15b80a26a0148beee6715ba61b30a0360c72300
Leturgone <finekon221@gmail.com>
2026-04-02 17:45:45 +0300

    add SortDirEmployeesByWorkloadUseCase

---
+++ app/src/main/java/com/app/masterplan/domain/useacse/employee/SortDirEmployeesByWorkloadUseCase.kt
@@ -0,0 +1,18 @@
+package com.app.masterplan.domain.useacse.employee
+
+import com.app.masterplan.domain.model.employee.Employee
+import com.app.masterplan.domain.repository.remote.EmployeeRepository
+import java.util.UUID
+
+class SortDirEmployeesByWorkloadUseCase(
+    private val employeeRepository: EmployeeRepository
+) {
+    suspend operator fun invoke(directorId: UUID): Result<List<Employee>>{
+        return try {
+            val list = employeeRepository.sortDirEmployeesByWorkload(directorId)
+            Result.success(list)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
