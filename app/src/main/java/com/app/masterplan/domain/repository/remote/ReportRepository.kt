737f887f0d789d2d6b2ce6da4f54c29195269f69
Leturgone <finekon221@gmail.com>
2026-04-03 15:10:40 +0300

    add methods to ReportRepository

--- app/src/main/java/com/app/masterplan/domain/repository/remote/ReportRepository.kt
+++ app/src/main/java/com/app/masterplan/domain/repository/remote/ReportRepository.kt
@@ -1,6 +1,8 @@
 package com.app.masterplan.domain.repository.remote
 
+import com.app.masterplan.domain.dto.AttachedDocument
 import com.app.masterplan.domain.dto.NewReportData
+import com.app.masterplan.domain.dto.UpdateReportData
 import com.app.masterplan.domain.model.reports.Report
 import com.app.masterplan.domain.model.reports.ReportStatus
 import com.app.masterplan.domain.model.reports.ReportType
@@ -10,7 +12,7 @@ interface ReportRepository {
 
     suspend fun updateReportStatus(reportId: UUID, status: ReportStatus, type: ReportType): UUID
 
-    suspend fun createReport(newReportData: NewReportData): UUID
+    suspend fun createReport(newReportData: NewReportData, document: AttachedDocument, reportType: ReportType): UUID
 
     suspend fun deleteReport(reportId: UUID, reportType: ReportType): UUID
 
@@ -18,5 +20,15 @@ interface ReportRepository {
                                                 status: ReportStatus,
                                                 reportType: ReportType): List<Report>
 
-    suspend fun getFilterByStatusSubordinatesTaskReports(directorId: UUID, status: ReportStatus): List<Report>
+    suspend fun getFilterByStatusSubordinatesTaskReports(directorId: UUID,
+                                                         status: ReportStatus): List<Report>
+
+    suspend fun getCreatedReports(employeeId: UUID, reportType: ReportType): List<Report>
+
+    suspend fun getReport(reportId: UUID, reportType: ReportType): Report
+
+    suspend fun getSubordinatesTaskReports(directorId: UUID): List<Report>
+
+    suspend fun updateReport(reportId: UUID, updatedData: UpdateReportData,
+                             reportType: ReportType, document: AttachedDocument): UUID
 }
