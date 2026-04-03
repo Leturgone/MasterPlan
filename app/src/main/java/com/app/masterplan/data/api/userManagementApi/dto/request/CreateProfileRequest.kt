3dde0f9cfe556288c4104c856bda61c1657d0da1
Leturgone <finekon221@gmail.com>
2026-04-03 19:59:35 +0300

    add CreateProfileRequest

---
+++ app/src/main/java/com/app/masterplan/data/api/userManagementApi/dto/request/CreateProfileRequest.kt
@@ -0,0 +1,14 @@
+package com.app.masterplan.data.api.userManagementApi.dto.request
+
+import java.util.UUID
+
+data class CreateProfileRequest(
+    val login: String,
+    val password: String,
+    val roles: Set<String>,
+    val name: String,
+    val surname: String,
+    val patronymic: String? = null,
+    val directorId: UUID? =  null
+
+)
