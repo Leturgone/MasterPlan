7c3f7de0c6423673d62a4dd0d13e9ca246c69d82
Leturgone <finekon221@gmail.com>
2026-04-03 15:09:54 +0300

    add document to CreateReportUseCase

--- app/src/main/java/com/app/masterplan/domain/useacse/reports/CreateReportUseCase.kt
+++ app/src/main/java/com/app/masterplan/domain/useacse/reports/CreateReportUseCase.kt
@@ -1,15 +1,20 @@
 package com.app.masterplan.domain.useacse.reports
 
+import com.app.masterplan.domain.dto.AttachedDocument
 import com.app.masterplan.domain.dto.NewReportData
+import com.app.masterplan.domain.model.reports.ReportType
 import com.app.masterplan.domain.repository.remote.ReportRepository
 import java.util.UUID
 
 class CreateReportUseCase(
     private val reportRepository: ReportRepository
 ) {
-    suspend operator fun invoke(newReportData: NewReportData): Result<UUID>{
+    suspend operator fun invoke(newReportData: NewReportData,
+                                document: AttachedDocument,
+                                reportType: ReportType
+                                ): Result<UUID>{
         return try {
-            val createdReportId = reportRepository.createReport(newReportData)
+            val createdReportId = reportRepository.createReport(newReportData,document,reportType)
             Result.success(createdReportId)
         }catch (e: Exception){
             Result.failure(e)
