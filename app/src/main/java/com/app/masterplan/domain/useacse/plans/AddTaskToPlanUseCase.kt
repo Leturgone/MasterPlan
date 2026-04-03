ee75beb115b9b993ffe43970b224f0ab4863dc7f
Leturgone <finekon221@gmail.com>
2026-04-03 15:09:12 +0300

    add document to AddTaskToPlanUseCase

--- app/src/main/java/com/app/masterplan/domain/useacse/plans/AddTaskToPlanUseCase.kt
+++ app/src/main/java/com/app/masterplan/domain/useacse/plans/AddTaskToPlanUseCase.kt
@@ -1,5 +1,6 @@
 package com.app.masterplan.domain.useacse.plans
 
+import com.app.masterplan.domain.dto.AttachedDocument
 import com.app.masterplan.domain.dto.NewTaskData
 import com.app.masterplan.domain.repository.remote.TaskRepository
 import java.util.UUID
@@ -7,9 +8,9 @@ import java.util.UUID
 class AddTaskToPlanUseCase(
     private val taskRepository: TaskRepository
 ) {
-    suspend operator fun invoke(planId: UUID, newTask: NewTaskData): Result<UUID>{
+    suspend operator fun invoke(planId: UUID, newTask: NewTaskData, document: AttachedDocument? = null,): Result<UUID>{
         return try {
-            val taskId = taskRepository.addTaskToPlan(planId,newTask)
+            val taskId = taskRepository.addTaskToPlan(planId,newTask,document)
             Result.success(taskId)
         }catch (e: Exception){
             Result.failure(e)
