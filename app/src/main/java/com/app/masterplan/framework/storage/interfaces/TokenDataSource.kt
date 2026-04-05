692143a242223ab5a50933d541f5449734d0181d
Leturgone <finekon221@gmail.com>
2026-04-05 20:03:29 +0300

    add TokenDataSource

--- app/src/main/java/com/app/masterplan/framework/storage/interfaces/TokenDataSource.kt
+++ app/src/main/java/com/app/masterplan/framework/storage/interfaces/TokenDataSource.kt
@@ -1,8 +1,10 @@
 package com.app.masterplan.framework.storage.interfaces
 
+import com.app.masterplan.domain.model.auth.JwtToken
+
 interface TokenDataSource {
 
-    suspend fun getTokenFromDataStorage(): String
+    suspend fun getTokenFromDataStorage(): JwtToken
 
-    suspend fun saveTokenToDataStorage()
+    suspend fun saveTokenToDataStorage(token: JwtToken)
 }
