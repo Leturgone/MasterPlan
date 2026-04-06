014965db796e32bd7b24e57d5c3af227ba52a0fb
Leturgone <finekon221@gmail.com>
2026-04-06 12:40:19 +0300

    change return type

--- app/src/main/java/com/app/masterplan/data/storage/LocalFileDataStorage.kt
+++ app/src/main/java/com/app/masterplan/data/storage/LocalFileDataStorage.kt
@@ -1,7 +1,9 @@
 package com.app.masterplan.data.storage
 
+import java.io.File
+
 interface LocalFileDataStorage {
 
-    suspend fun saveFileToDataStorage(byteArray: ByteArray): String
+    suspend fun saveFileToDataStorage(byteArray: ByteArray): File
 
 }
