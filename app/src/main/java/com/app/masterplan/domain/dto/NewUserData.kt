ced5ff21adb8de8697281b555294c1c8beb74950
Leturgone <finekon221@gmail.com>
2026-04-06 23:06:32 +0300

    change set to list in NewUserData

--- app/src/main/java/com/app/masterplan/domain/dto/NewUserData.kt
+++ app/src/main/java/com/app/masterplan/domain/dto/NewUserData.kt
@@ -5,7 +5,7 @@ import com.app.masterplan.domain.model.userManagement.UserRole
 data class NewUserData(
     val login: String,
     val password: String,
-    val roles: Set<UserRole>,
+    val roles: List<UserRole>,
     val employeeInfo: NewEmployeeData
 )
 
