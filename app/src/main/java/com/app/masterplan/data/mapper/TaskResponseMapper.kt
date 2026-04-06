7d0457dd63eacf34a2dc3c545b45e85cc7062c10
Leturgone <finekon221@gmail.com>
2026-04-06 17:54:51 +0300

    add TaskResponseMapper

---
+++ app/src/main/java/com/app/masterplan/data/mapper/TaskResponseMapper.kt
@@ -0,0 +1,37 @@
+package com.app.masterplan.data.mapper
+
+import com.app.masterplan.data.api.plansApi.dto.responce.TaskIdResponse
+import com.app.masterplan.data.api.plansApi.dto.responce.TaskInformationResponse
+import com.app.masterplan.domain.model.plans.Task
+import com.app.masterplan.domain.model.plans.TaskStatus
+
+object TaskResponseMapper {
+
+    fun toDomain(response: TaskIdResponse) = response.id
+
+    fun toDomain(response: TaskInformationResponse): Task {
+
+        val status = try {
+            TaskStatus.valueOf(response.status)
+        }catch (_: Exception){
+            throw Exception("Invalid status: ${response.status}")
+        }
+
+        return Task(
+            id = response.id,
+            title = response.title,
+            description = response.description,
+            urgency = response.urgency,
+            endDate = response.endDate,
+            status = status,
+            planId = response.planId,
+            documentId = response.documentId,
+            executorsIds = response.executorsIds
+        )
+    }
+
+
+    fun toDomain(response: List<TaskInformationResponse>): List<Task>{
+        return response.map { toDomain(it) }
+    }
+}
