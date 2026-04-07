780137baa0f9a6647dc6de1329fc869c925c9177
Leturgone <finekon221@gmail.com>
2026-04-08 01:41:16 +0300

    add NotificationCreator

---
+++ app/src/main/java/com/app/masterplan/framework/service/NotificationCreator.kt
@@ -0,0 +1,42 @@
+package com.app.masterplan.framework.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import com.app.masterplan.R
+import com.app.masterplan.domain.model.notification.PushNotification
+import javax.inject.Inject
+
+class NotificationCreator @Inject constructor(
+    private val context: Context,
+) {
+
+    private var notificationManager: NotificationManager = context.getSystemService(
+        Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    private val channelId = "masterplan_notifications"
+
+    init {
+        createNotificationChannel()
+
+    }
+
+    private fun createNotificationChannel(){
+        val channel = NotificationChannel(
+            channelId,"Уведомления", NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    fun showNotification(notification: PushNotification){
+        val builder = NotificationCompat.Builder(context,channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(notification.title)
+            .setContentText(notification.message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        notificationManager.notify(notification.notificationId, builder.build())
+    }
+}
