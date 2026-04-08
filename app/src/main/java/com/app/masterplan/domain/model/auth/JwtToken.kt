115c1bfd30edbeb1868cc5452f127be722f7961b
Leturgone <finekon221@gmail.com>
2026-04-08 18:53:43 +0300

    add id to jwt

--- app/src/main/java/com/app/masterplan/domain/model/auth/JwtToken.kt
+++ app/src/main/java/com/app/masterplan/domain/model/auth/JwtToken.kt
@@ -1,8 +1,10 @@
 package com.app.masterplan.domain.model.auth
 
 import com.app.masterplan.domain.model.userManagement.UserRole
+import java.util.UUID
 
 data class JwtToken(
     val token: String,
-    val roles: Set<UserRole>
+    val roles: Set<UserRole>,
+    val id: UUID
 )
