8a42dafbc7dac6cf58ec3e9fb85571c8bac31441
Leturgone <finekon221@gmail.com>
2026-04-08 15:57:31 +0300

    add NotificationAdapter with startService

--- app/src/main/java/com/app/masterplan/framework/adapter/NotificationAdapter.kt
+++ app/src/main/java/com/app/masterplan/framework/adapter/NotificationAdapter.kt
@@ -1,60 +1,34 @@
 package com.app.masterplan.framework.adapter
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
-import androidx.core.app.NotificationCompat
-import com.app.masterplan.R
-import com.app.masterplan.domain.model.notification.PushNotification
+import android.content.Intent
+import com.app.masterplan.domain.model.userManagement.UserRole
 import com.app.masterplan.domain.ports.NotificationPort
-import com.app.masterplan.framework.websocket.WebSocketWorker
+import com.app.masterplan.framework.service.WebsocketNotificationService
 import java.util.UUID
 import javax.inject.Inject
 
 class NotificationAdapter @Inject constructor(
-    private val context: Context,
-    private val webSocketWorker: WebSocketWorker
+    private val context: Context
 ): NotificationPort {
 
-    private var notificationManager: NotificationManager = context.getSystemService(
-        Context.NOTIFICATION_SERVICE) as NotificationManager
 
-    private val channelId = "masterplan_notifications"
-
-    init {
-        createNotificationChannel()
-
-    }
-
-    override suspend fun subscribeToUserNotifications(userId: UUID) {
-        webSocketWorker.connectForUser(userId) { notificationData ->
-            showNotification(notificationData)
-
+    override suspend fun subscribeToUserNotifications(userRole: UserRole,userId: UUID) {
+        val intent = Intent(context, WebsocketNotificationService::class.java).apply {
+            action = WebsocketNotificationService.ACTION_SUBSCRIBE
+            putExtra("USER_ID",userId)
+            putExtra("USER_ROLE",userRole)
         }
+        context.startService(intent)
+
+
     }
 
     override suspend fun unsubscribeFromNotifications() {
-        webSocketWorker.disconnect()
+        val intent = Intent(context, WebsocketNotificationService::class.java).apply {
+            action = WebsocketNotificationService.ACTION_UNSUBSCRIBE
+        }
+        context.startService(intent)
     }
 
-
-    private fun createNotificationChannel(){
-        val channel = NotificationChannel(
-            channelId,"Уведомления", NotificationManager.IMPORTANCE_DEFAULT
-        )
-        notificationManager.createNotificationChannel(channel)
-    }
-
-
-
-    private fun showNotification(notification: PushNotification){
-        val builder = NotificationCompat.Builder(context,channelId)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle(notification.title)
-            .setContentText(notification.message)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setAutoCancel(true)
-
-        notificationManager.notify(notification.notificationId, builder.build())
-    }
 }
