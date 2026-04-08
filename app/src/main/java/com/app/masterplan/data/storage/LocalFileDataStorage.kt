fa34d7342d5a7e42e1f7e9faff9a6a2b872f48c9
Leturgone <finekon221@gmail.com>
2026-04-08 21:37:39 +0300

    add getFileByPath method to Storage

--- app/src/main/java/com/app/masterplan/data/storage/LocalFileDataStorage.kt
+++ app/src/main/java/com/app/masterplan/data/storage/LocalFileDataStorage.kt
@@ -1,9 +1,12 @@
 package com.app.masterplan.data.storage
 
+import com.app.masterplan.domain.dto.AttachedDocument
 import java.io.File
 
 interface LocalFileDataStorage {
 
     suspend fun saveFileToDataStorage(byteArray: ByteArray): File
+
+    suspend fun getFileByPath(filePath: String): AttachedDocument
 
 }
