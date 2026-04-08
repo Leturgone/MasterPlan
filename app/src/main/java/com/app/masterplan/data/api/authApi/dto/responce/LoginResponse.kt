373e6f865ce73cc9a80ba9b3ff7e386eaf9d0941
Leturgone <finekon221@gmail.com>
2026-04-08 18:53:50 +0300

    add id to jwt

--- app/src/main/java/com/app/masterplan/data/api/authApi/dto/responce/LoginResponse.kt
+++ app/src/main/java/com/app/masterplan/data/api/authApi/dto/responce/LoginResponse.kt
@@ -1,9 +1,12 @@
 package com.app.masterplan.data.api.authApi.dto.responce
 
+import java.util.UUID
+
 data class LoginResponse(
     val token: String,
     val type: String = "Bearer",
-    val roles: List<String>
+    val roles: List<String>,
+    val id: UUID
 )
 
 
