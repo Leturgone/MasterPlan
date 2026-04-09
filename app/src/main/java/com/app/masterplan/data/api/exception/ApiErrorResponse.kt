e5d52ba1b7522d958e2d4fa0f9895b8b3443ee39
Leturgone <finekon221@gmail.com>
2026-04-09 23:36:43 +0300

    change date in ApiErrorResponse to string

--- app/src/main/java/com/app/masterplan/data/api/exception/ApiErrorResponse.kt
+++ app/src/main/java/com/app/masterplan/data/api/exception/ApiErrorResponse.kt
@@ -1,9 +1,7 @@
 package com.app.masterplan.data.api.exception
 
-import java.time.LocalDateTime
-
 data class ApiErrorResponse(
     val status: Int,
     val message: String? = "",
-    val timestamp: LocalDateTime,
+    val timestamp: String,
 )
