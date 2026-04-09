46a4d74aa993f63fa5403c76e530866edb24a597
Leturgone <finekon221@gmail.com>
2026-04-09 23:43:07 +0300

    edit error mapping in ApiErrorResponseHandler

--- app/src/main/java/com/app/masterplan/data/mapper/ApiErrorResponseHandler.kt
+++ app/src/main/java/com/app/masterplan/data/mapper/ApiErrorResponseHandler.kt
@@ -1,30 +1,32 @@
 package com.app.masterplan.data.mapper
 
+import android.util.Log
 import com.app.masterplan.data.api.exception.ApiErrorResponse
 import com.app.masterplan.data.exception.ApiException
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import retrofit2.Response
 import java.time.LocalDateTime
 
-object ApiErrorHandler {
+object ApiErrorResponseHandler {
     fun  <T> handleResponse(
         response: Response<T>,
         errorMapper: (ApiErrorResponse) -> ApiException
     ): T {
-        val body = response.body() ?: throw NullPointerException("Response body is null")
         return if (response.isSuccessful){
-            body
+            response.body() ?: throw NullPointerException("Response body is null")
         }
         else{
             try {
                 val gson = Gson()
-                val errorResponse = gson.fromJson(body.toString(), ApiErrorResponse::class.java)
+                val body = response.errorBody()?.string() ?: throw NullPointerException("Error body is null")
+                val errorResponse = gson.fromJson(body, ApiErrorResponse::class.java)
                 throw errorMapper(errorResponse)
-            }catch (e: Exception){
+            }catch (e: JsonSyntaxException){
+                Log.e("ApiErrorResponseHandler",e.message.toString())
                 throw ApiException.AdminApiException(
                     status = -1,
-                    apiMessage = "Failed to parse admin error: ${e.message}",
-                    timestamp = LocalDateTime.now()
+                    apiMessage = "Failed to parse admin error: ${e.message}"
                 )
             }
         }
