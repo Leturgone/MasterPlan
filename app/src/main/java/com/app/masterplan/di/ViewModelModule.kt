fbda4dbd1c2f629e1252ace6d64f4cd5ed3b48c4
Leturgone <finekon221@gmail.com>
2026-04-10 20:22:11 +0300

    add new dep in di

--- app/src/main/java/com/app/masterplan/di/ViewModelModule.kt
+++ app/src/main/java/com/app/masterplan/di/ViewModelModule.kt
@@ -1,8 +1,11 @@
 package com.app.masterplan.di
 
 import com.app.masterplan.domain.repository.remote.AuthRepository
+import com.app.masterplan.domain.useacse.auth.GetUserIdUseCase
+import com.app.masterplan.domain.useacse.auth.GetUserRoleUseCase
 import com.app.masterplan.domain.useacse.auth.LoginUseCase
 import com.app.masterplan.presentation.ui.auth.viewModel.LoginScreenViewModel
+import com.app.masterplan.presentation.ui.bottomBar.viewModel.BottomBarViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,6 +23,18 @@ object ViewModelModule {
         authRepository: AuthRepository
     ): LoginScreenViewModel {
         val loginUseCase = LoginUseCase(authRepository)
-        return LoginScreenViewModel(loginUseCase)
+        val getUserIdUseCase = GetUserIdUseCase(authRepository)
+        val getUserRoleUseCase = GetUserRoleUseCase(authRepository)
+        return LoginScreenViewModel(loginUseCase, getUserIdUseCase, getUserRoleUseCase)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideBottomBarViewModel(
+        authRepository: AuthRepository
+    ): BottomBarViewModel {
+        val getUserRoleUseCase = GetUserRoleUseCase(authRepository)
+        return BottomBarViewModel(getUserRoleUseCase)
     }
 }
