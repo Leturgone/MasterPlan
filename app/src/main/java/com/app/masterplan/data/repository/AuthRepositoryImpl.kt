811281ede7c4e25e24745ddf6225f14c4320bddf
Leturgone <finekon221@gmail.com>
2026-04-10 20:19:07 +0300

    add implementations with cashing

--- app/src/main/java/com/app/masterplan/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/app/masterplan/data/repository/AuthRepositoryImpl.kt
@@ -6,15 +6,22 @@ import com.app.masterplan.data.api.exception.ApiErrorResponse
 import com.app.masterplan.data.exception.ApiException
 import com.app.masterplan.data.mapper.ApiErrorResponseHandler
 import com.app.masterplan.data.mapper.AuthResponseMapper
-import com.app.masterplan.domain.model.auth.JwtToken
-import com.app.masterplan.domain.repository.remote.AuthRepository
 import com.app.masterplan.data.storage.TokenDataStorage
+import com.app.masterplan.domain.model.auth.JwtToken
+import com.app.masterplan.domain.model.userManagement.UserRole
+import com.app.masterplan.domain.repository.remote.AuthRepository
+import java.util.UUID
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val api: AuthApi,
     private val tokenStorage: TokenDataStorage
 ): AuthRepository {
+
+    private var userIdCache: UUID? = null
+
+    private var rolesCache: Set<UserRole>? = null
+
     override suspend fun login(
         login: String,
         password: String
@@ -33,6 +40,24 @@ class AuthRepositoryImpl @Inject constructor(
 
     override suspend fun logout() {
         tokenStorage.removeTokenFromDataStorage()
+    }
+
+    override suspend fun getLocalRoles(): Set<UserRole> {
+        return rolesCache ?: run {
+            val token = tokenStorage.getTokenFromDataStorage()
+            val roles = token.roles
+            rolesCache = roles
+            roles
+        }
+    }
+
+    override suspend fun getLocalUserId(): UUID {
+        return userIdCache ?: run {
+            val token = tokenStorage.getTokenFromDataStorage()
+            val userId = token.id
+            userIdCache = userId
+            userId
+        }
     }
 
     private fun errorMapper(errorResp: ApiErrorResponse): ApiException.AuthApiException {
