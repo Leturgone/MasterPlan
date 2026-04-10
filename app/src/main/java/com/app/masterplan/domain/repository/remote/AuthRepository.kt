4a19bcb2dd7f9aa81ef7661d197c866041e76cf6
Leturgone <finekon221@gmail.com>
2026-04-10 20:18:45 +0300

    add getLocalRoles() and getLocalUserId()

--- app/src/main/java/com/app/masterplan/domain/repository/remote/AuthRepository.kt
+++ app/src/main/java/com/app/masterplan/domain/repository/remote/AuthRepository.kt
@@ -1,9 +1,15 @@
 package com.app.masterplan.domain.repository.remote
 
 import com.app.masterplan.domain.model.auth.JwtToken
+import com.app.masterplan.domain.model.userManagement.UserRole
+import java.util.UUID
 
 interface AuthRepository {
     suspend fun login(login: String, password: String): JwtToken
 
     suspend fun logout()
+
+    suspend fun getLocalRoles(): Set<UserRole>
+
+    suspend fun getLocalUserId(): UUID
 }
