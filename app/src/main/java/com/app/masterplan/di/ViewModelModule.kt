a54912425c60242326ee0edf071464685599213d
Leturgone <finekon221@gmail.com>
2026-04-11 20:13:35 +0300

    add RequestCardViewModel to module

--- app/src/main/java/com/app/masterplan/di/ViewModelModule.kt
+++ app/src/main/java/com/app/masterplan/di/ViewModelModule.kt
@@ -3,13 +3,17 @@ package com.app.masterplan.di
 import com.app.masterplan.domain.repository.remote.AdminRequestsRepository
 import com.app.masterplan.domain.repository.remote.AuthRepository
 import com.app.masterplan.domain.repository.remote.EmployeeRepository
+import com.app.masterplan.domain.useacse.adminRequests.ChangeAdminRequestStatusUseCase
+import com.app.masterplan.domain.useacse.adminRequests.GetAdminAnswerForRequestUseCase
 import com.app.masterplan.domain.useacse.adminRequests.GetAdminRequestsListUseCase
 import com.app.masterplan.domain.useacse.adminRequests.GetCreatedAdminRequestsBySenderListUseCase
 import com.app.masterplan.domain.useacse.employee.GetLocalEmpIdUseCase
 import com.app.masterplan.domain.useacse.auth.GetUserRoleUseCase
 import com.app.masterplan.domain.useacse.auth.LoginUseCase
+import com.app.masterplan.domain.useacse.employee.GetEmployeeByIdUseCase
 import com.app.masterplan.presentation.ui.auth.viewModel.LoginScreenViewModel
 import com.app.masterplan.presentation.ui.bottomBar.viewModel.BottomBarViewModel
+import com.app.masterplan.presentation.ui.requests.viewmodel.RequestCardViewModel
 import com.app.masterplan.presentation.ui.requests.viewmodel.RequestsListScreenViewModel
 import dagger.Module
 import dagger.Provides
@@ -63,5 +67,23 @@ object ViewModelModule {
             getUserRoleUseCase
         )
     }
+
+    @Provides
+    @Singleton
+    fun provideRequestCardViewModel(
+        employeeRepository: EmployeeRepository,
+        adminRequestsRepository: AdminRequestsRepository,
+        authRepository: AuthRepository
+    ): RequestCardViewModel {
+        val getUserRoleUseCase = GetUserRoleUseCase(authRepository)
+        val updateAdminRequestStatusUseCase = ChangeAdminRequestStatusUseCase(adminRequestsRepository)
+        val getAdminAnswerForRequestUseCase = GetAdminAnswerForRequestUseCase(adminRequestsRepository)
+        val getEmployeeByIdUseCase = GetEmployeeByIdUseCase(employeeRepository)
+        return RequestCardViewModel(
+            getUserRoleUseCase, updateAdminRequestStatusUseCase,
+            getAdminAnswerForRequestUseCase,  getEmployeeByIdUseCase
+        )
+    }
+
 
 }
