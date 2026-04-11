fe7ad9f11322904b9ee962906fa958ba3832ab7b
Leturgone <finekon221@gmail.com>
2026-04-11 16:10:00 +0300

    remove unused

--- app/src/main/java/com/app/masterplan/domain/repository/remote/AuthRepository.kt
+++ app/src/main/java/com/app/masterplan/domain/repository/remote/AuthRepository.kt
@@ -2,7 +2,6 @@ package com.app.masterplan.domain.repository.remote
 
 import com.app.masterplan.domain.model.auth.JwtToken
 import com.app.masterplan.domain.model.userManagement.UserRole
-import java.util.UUID
 
 interface AuthRepository {
     suspend fun login(login: String, password: String): JwtToken
@@ -10,6 +9,4 @@ interface AuthRepository {
     suspend fun logout()
 
     suspend fun getLocalRoles(): Set<UserRole>
-
-    suspend fun getLocalUserId(): UUID
 }
