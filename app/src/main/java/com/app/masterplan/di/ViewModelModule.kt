58e7c3a964418632770be68c62e54244d470197f
Leturgone <finekon221@gmail.com>
2026-04-12 17:39:26 +0300

    add provides for view models

--- app/src/main/java/com/app/masterplan/di/ViewModelModule.kt
+++ app/src/main/java/com/app/masterplan/di/ViewModelModule.kt
@@ -3,6 +3,8 @@ package com.app.masterplan.di
 import com.app.masterplan.domain.repository.remote.AdminRequestsRepository
 import com.app.masterplan.domain.repository.remote.AuthRepository
 import com.app.masterplan.domain.repository.remote.EmployeeRepository
+import com.app.masterplan.domain.repository.remote.SearchHistoryRepository
+import com.app.masterplan.domain.repository.remote.UserRepository
 import com.app.masterplan.domain.useacse.adminRequests.ChangeAdminRequestStatusUseCase
 import com.app.masterplan.domain.useacse.adminRequests.CreateAdminAnswerUseCase
 import com.app.masterplan.domain.useacse.adminRequests.CreateAdminRequestUseCase
@@ -12,7 +14,16 @@ import com.app.masterplan.domain.useacse.adminRequests.GetCreatedAdminRequestsBy
 import com.app.masterplan.domain.useacse.employee.GetLocalEmpIdUseCase
 import com.app.masterplan.domain.useacse.auth.GetUserRoleUseCase
 import com.app.masterplan.domain.useacse.auth.LoginUseCase
+import com.app.masterplan.domain.useacse.employee.GetAllEmployeesUseCase
 import com.app.masterplan.domain.useacse.employee.GetEmployeeByIdUseCase
+import com.app.masterplan.domain.useacse.employee.SearchEmployeeByNameUseCase
+import com.app.masterplan.domain.useacse.searchHistory.ClearSearchHistoryUseCase
+import com.app.masterplan.domain.useacse.searchHistory.GetSearchHistoryUseCase
+import com.app.masterplan.domain.useacse.searchHistory.SaveSearchHistoryUseCase
+import com.app.masterplan.domain.useacse.userManagement.DeleteUserUseCase
+import com.app.masterplan.domain.useacse.userManagement.GetUserByIdUseCase
+import com.app.masterplan.presentation.ui.accounts.viewmodel.AccountCardViewModel
+import com.app.masterplan.presentation.ui.accounts.viewmodel.AccountListViewModel
 import com.app.masterplan.presentation.ui.auth.viewModel.LoginScreenViewModel
 import com.app.masterplan.presentation.ui.bottomBar.viewModel.BottomBarViewModel
 import com.app.masterplan.presentation.ui.requests.viewmodel.NewAnswerScreenViewModel
@@ -111,6 +122,42 @@ object ViewModelModule {
         val createAdminRequestUseCase = CreateAdminRequestUseCase(adminRequestsRepository)
         return NewRequestsScreenViewModel(
             getLocalEmpIdUseCase, createAdminRequestUseCase
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideAccountCardViewModel(
+        employeeRepository: EmployeeRepository,
+        userRepository: UserRepository
+    ): AccountCardViewModel {
+        val getUserByIdUseCase = GetUserByIdUseCase(userRepository)
+        val getEmployeeByIdUseCase = GetEmployeeByIdUseCase(employeeRepository)
+        val deleteUserUseCase = DeleteUserUseCase(userRepository)
+        return AccountCardViewModel(
+            getUserByIdUseCase,
+            getEmployeeByIdUseCase,
+            deleteUserUseCase
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideAccountListViewModel(
+        employeeRepository: EmployeeRepository,
+        searchHistoryRepository: SearchHistoryRepository,
+    ): AccountListViewModel {
+        val getAllEmployeesUseCase = GetAllEmployeesUseCase(employeeRepository)
+        val getSearchHistoryUseCase = GetSearchHistoryUseCase(searchHistoryRepository)
+        val saveSearchHistoryUseCase = SaveSearchHistoryUseCase(searchHistoryRepository)
+        val clearSearchHistoryUseCase = ClearSearchHistoryUseCase(searchHistoryRepository)
+        val searchEmployeeByNameUseCase = SearchEmployeeByNameUseCase(employeeRepository)
+        return AccountListViewModel(
+            getAllEmployeesUseCase,
+            getSearchHistoryUseCase,
+            saveSearchHistoryUseCase,
+            clearSearchHistoryUseCase,
+            searchEmployeeByNameUseCase
         )
     }
 
