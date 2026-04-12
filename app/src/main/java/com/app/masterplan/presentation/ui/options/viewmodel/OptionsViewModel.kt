7babef887ab823c12d28dbb14f819bb70c4f89fd
Leturgone <finekon221@gmail.com>
2026-04-13 02:43:58 +0300

    add OptionsViewModel

---
+++ app/src/main/java/com/app/masterplan/presentation/ui/options/viewmodel/OptionsViewModel.kt
@@ -0,0 +1,43 @@
+package com.app.masterplan.presentation.ui.options.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.app.masterplan.domain.useacse.auth.LogoutUseCase
+import com.app.masterplan.domain.useacse.theme.ChangeThemeUseCase
+import com.app.masterplan.domain.useacse.theme.GetCurrentThemeIsDarkUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OptionsViewModel @Inject constructor(
+    private val changeThemeUseCase: ChangeThemeUseCase,
+    private val getCurrentThemeIsDarkUseCase: GetCurrentThemeIsDarkUseCase,
+    private val logoutUseCase: LogoutUseCase
+): ViewModel(){
+
+    private val _isDarkMode = MutableStateFlow(false)
+    val isDarkMode:StateFlow<Boolean> = _isDarkMode
+
+    init {
+        viewModelScope.launch {
+            _isDarkMode.value = getCurrentThemeIsDarkUseCase().getOrElse {
+                false
+            }
+        }
+
+    }
+
+
+    fun switchTheme() = viewModelScope.launch {
+        val currentTheme = _isDarkMode.value
+        _isDarkMode.value = !currentTheme
+        changeThemeUseCase(!currentTheme)
+    }
+
+    fun logout() = viewModelScope.launch {
+        logoutUseCase()
+    }
+}
