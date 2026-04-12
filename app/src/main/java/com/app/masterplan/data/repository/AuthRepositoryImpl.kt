4945a5bf32a1f71482db7062959023299b89493a
Leturgone <finekon221@gmail.com>
2026-04-13 02:45:05 +0300

    add clearing history whilt logout

--- app/src/main/java/com/app/masterplan/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/app/masterplan/data/repository/AuthRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.app.masterplan.data.api.exception.ApiErrorResponse
 import com.app.masterplan.data.exception.ApiException
 import com.app.masterplan.data.mapper.ApiErrorResponseHandler
 import com.app.masterplan.data.mapper.AuthResponseMapper
+import com.app.masterplan.data.storage.SearchHistoryDataStorage
 import com.app.masterplan.data.storage.TokenDataStorage
 import com.app.masterplan.domain.model.auth.JwtToken
 import com.app.masterplan.domain.model.userManagement.UserRole
@@ -14,7 +15,8 @@ import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val api: AuthApi,
-    private val tokenStorage: TokenDataStorage
+    private val tokenStorage: TokenDataStorage,
+    private val searchHistoryStorage: SearchHistoryDataStorage
 ): AuthRepository {
 
 
@@ -38,6 +40,8 @@ class AuthRepositoryImpl @Inject constructor(
 
     override suspend fun logout() {
         tokenStorage.removeTokenFromDataStorage()
+        searchHistoryStorage.clearSearchHistory()
+
     }
 
     override suspend fun getLocalRoles(): Set<UserRole> {
